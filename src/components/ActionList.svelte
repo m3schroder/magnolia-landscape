8a00828e599e32629d6a17197a5b1cb897464da5
Matt S <m3schroder@gmail.com>
2023-01-29 21:11:18 -0600

    done

--- src/components/ActionList.svelte
+++ src/components/ActionList.svelte
@@ -5,11 +5,11 @@
 	export let title: string;
 </script>
 
-<Card class="action-card">
-	<h3 class="text-lg text-white font-semibold text-center">{title}</h3>
-	<ul>
+<Card class="card flex flex-col text-neutral-700 !bg-blue-200 items-start !px-12 w-fit">
+	<h6 class="text-lg lg:text-xl lg:mb-6">{title}</h6>
+	<ul class="flex flex-col gap-2 font-light list-disc text-lg">
 		{#each services as service}
-			<li>{service}</li>
+			<li class="pl-2 mx-5">{service}</li>
 		{/each}
 	</ul>
 </Card>
